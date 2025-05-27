8eeb1faec1fc231575d5398bfd0cc7ac88306098
João Pedro Carneiro <107374203+crnvsk@users.noreply.github.com>
2025-05-27 15:00:55 -0300

    add: prova de conceito de rate limit

--- distributedpoc/serviceb/src/main/java/distributedpoc/serviceb/CallAController.java
+++ distributedpoc/serviceb/src/main/java/distributedpoc/serviceb/CallAController.java
@@ -1,11 +1,13 @@
 package distributedpoc.serviceb;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -28,12 +30,20 @@ public class CallAController {
     @GetMapping("/call-a")
     @CircuitBreaker(name = "serviceA", fallbackMethod = "fallbackCallA")
     @Retry(name = "serviceA")
-    public String callA() {
+    @RateLimiter(name = "callA", fallbackMethod = "rateLimitFallback")
+    public String callA(@RequestParam(required = false, defaultValue = "false") boolean fail) {
+        if (fail) {
+            throw new RuntimeException("Falha simulada!");
+        }
         String response = restTemplate.getForObject("http://localhost:8081/hello", String.class);
         return "Service B recebeu: " + response;
     }
 
     public String fallbackCallA(Throwable t) {
         return "Service B: Service A está indisponível (fallback)";
+    }
+
+    public String rateLimitFallback(boolean fail, Throwable t) {
+        return "Service B: Limite de requisições atingido (Rate Limit)";
     }
 }
