2e1d7def3ca49eb505f89897d6bd2cf6e6bd5d68
João Pedro Carneiro <107374203+crnvsk@users.noreply.github.com>
2025-05-27 14:41:11 -0300

    add: prova de conceito de circuit breaker

--- distributedpoc/serviceb/src/main/java/distributedpoc/serviceb/CallAController.java
+++ distributedpoc/serviceb/src/main/java/distributedpoc/serviceb/CallAController.java
@@ -1,16 +1,36 @@
 package distributedpoc.serviceb;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
+class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
+
 @RestController
 public class CallAController {
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
+
+    public CallAController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @GetMapping("/call-a")
+    @CircuitBreaker(name = "serviceA", fallbackMethod = "fallbackCallA")
     public String callA() {
         String response = restTemplate.getForObject("http://localhost:8081/hello", String.class);
         return "Service B recebeu: " + response;
+    }
+
+    public String fallbackCallA(Throwable t) {
+        return "Service B: Service A está indisponível (fallback)";
     }
 }
