7501b23f258f1fc0e728054c9f7ac6e2656f6994
João Pedro Carneiro <107374203+crnvsk@users.noreply.github.com>
2025-06-04 16:29:11 -0300

    remove: remocao de pacotes que foram passados para repositorios proprios

--- service-microprofile/src/test/java/br/com/gubee/HelloResourceIT.java
+++
@@ -1,8 +0,0 @@
-package br.com.gubee;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class HelloResourceIT extends HelloResourceTest {
-    // Execute the same tests but in packaged mode.
-}
